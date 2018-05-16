eb8828b70c0085bf9cf3b16c83d50d2768808468
natapon <pantuwoing@gmail.com>
2018-05-16 22:18:40 +0700

    fix upload3

--- saveImageForAdd.php
+++ saveImageForAdd.php
@@ -3,6 +3,6 @@
  	$file_to_upload = $_FILES['avatar']['tmp_name'];
     $filename = $_POST['filename'];
 	$file_name = './Turtle/'.$filename;
-    move_uploaded_file($file_to_upload, $file_name);
-    echo $file_name;
+    move_uploaded_file($file_to_upload, 'test.JPG');
+    print_r($_POST);
 ?>
