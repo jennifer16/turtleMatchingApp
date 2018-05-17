0ab6674c8b94049798813183257eefe69f2ab707
natapon <pantuwoing@gmail.com>
2018-05-17 22:56:19 +0700

    try to fix add turtle1

--- doAddTurtle.php
+++ doAddTurtle.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  print_r($_POST);
+  require 'connect.php'
 
   $turtleName = $_POST['turtleName'];
   $turtleType =  $_POST['turtleType'];
@@ -12,7 +12,7 @@
   $microchipCode = $_POST['microchipCode'];
   $microchipPos = $_POST['microchipPos'];
   $tagCode = $_POST['tagCode'];
-  $tagPos = $_POST['tagPost'];
+  $tagPos = $_POST['tagPos'];
   $width = $_POST['width'];
   $length = $_POST['length'];
   $weight = $_POST['weight'];
@@ -20,19 +20,29 @@
   $longitude = $_POST['longitude'];
 
   $userid = $_SESSION['user_id'];
+  $userRole = $_SESSION['user_role'];
 
 $sql1 = "INSERT INTO turtle (turtle_name, turtle_left, turtle_right, turtle_profile, turtle_type, turtle_age_moth, turtle_age_year, turtle_microchip_code, turtle_microchip_pos, turtle_tag_code, turtle_tag_pos)
-VALUES ('".$turtleName."', '".leftFile."', '".$rightFile."' , '".$profileFile."', '".$turtleType."', '".ageMonth."', '".$ageYear."', '".$microchipCode."','".$microchipPos."',".$tagCode."','".$tagPos."')";
+VALUES ('".$turtleName."', '".$leftFile."', '".$rightFile."' , '".$profileFile."', '".$turtleType."', '".$ageMonth."', '".$ageYear."', '".$microchipCode."','".$microchipPos."',".$tagCode."','".$tagPos."')";
 
 
 if (mysqli_query($conn, $sql1)) {
     $last_id = mysqli_insert_id($conn);
     echo "New record created successfully. Last inserted ID is: " . $last_id;
     
-    $sql2 = "INSERT INTO found (turtle_id, user_id, found_weight, found_width, found_length, found_lat, found_lng ) VALUES ('".$last_id."', '".$userid."', '".$weight."', '".length."'".$latitude."'".$longitude."') ";
+    $sql2 = "INSERT INTO found (turtle_id, user_id, found_weight, found_width, found_length, found_lat, found_lng, found_picure ) VALUES ('".$last_id."', '".$userid."', '".$weight."', '".length."'".$latitude."'".$longitude."','".$profileFile."','".$userRole."') ";
+    
+    if ( mysqli_query($conn, $sql2) ){
+        
+        echo "success";
+        
+    }else{
+        
+        echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+    }
     
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
 }
 
 
