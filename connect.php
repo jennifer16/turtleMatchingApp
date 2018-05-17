6ccd3c15197a14eb1b6d758c7f8b5e9e82e8b1b9
natapon <pantuwoing@gmail.com>
2018-05-18 01:03:49 +0700

    fix lang2

--- connect.php
+++ connect.php
@@ -5,6 +5,5 @@ $password = "Tu2tlem@tching";
 $dbname = "turtle";
 $conn = mysqli_connect($host, $username, $password, $dbname );
 mysqli_set_charset('utf8');
-mysqli_query("SET collation_connection = utf8_unicode_ci");
 ?>
 
