9a42236c74a641dd58cf8cfef23a87f611b30765
natapon <pantuwoing@gmail.com>
2018-05-18 03:18:52 +0700

    detail

--- allTurtle.php
+++ allTurtle.php
@@ -173,7 +173,7 @@ echo mysqli_num_rows($data);
                     echo "<td align='center'><a href='turtleDetail.php?id=".$row['turtle_id']."'>".$row['turtle_name']."</a></td>";
                     echo "<td align='center'>".$row['turtle_microchip_code']."</td>";
                     echo "<td align='center'>".$row['turtle_tag_code']."</td>";
-                   echo "<td align='center'><button type='button' class='btn btn-info' onclick='goToDetail(".$row['turtle_id'].");'>ดูข้อมูล</button></td>";
+                   echo "<td align='center'><button type='button' class='btn btn-info' onclick='goToDetail(".$row['turtle_id'].");'>ดู</button></td>";
                     echo "</tr>";
 }
                     
