0a740aa9bad8b19441e1d3a7f4d2203459003da5
pantuwong <pantuwong@turtle.c.elite-pointer-201606.internal>
2018-05-17 01:33:58 +0000

    fix upload success

--- saveImageForAdd.php
+++ saveImageForAdd.php
@@ -3,6 +3,6 @@
  	$file_to_upload = $_FILES['avatarLeft']['tmp_name'];
     $filename = $_POST['filenameLeft'];
 	$file_name = './Turtle/'.$filename;
-    move_uploaded_file($file_to_upload, 'test.JPG');
+    move_uploaded_file($file_to_upload, $file_name);
     print_r($file_name);
 ?>
