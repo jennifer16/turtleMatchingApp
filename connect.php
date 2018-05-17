75093e403df4481e964415fd5d48d6fb4940f034
natapon <pantuwoing@gmail.com>
2018-05-18 01:09:32 +0700

    fix lang3

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $username = "root";
 $password = "Tu2tlem@tching";
 $dbname = "turtle";
-$conn = mysqli_connect($host, $username, $password, $dbname );
+$conn = new mysqli($host, $username, $password, $dbname );
 mysqli_set_charset('utf8');
 ?>
 
