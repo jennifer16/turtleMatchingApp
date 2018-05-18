fd9afa9b74ab80d43cd755258dcdab922588e9b9
natapon <pantuwoing@gmail.com>
2018-05-18 12:39:13 +0700

    fetch

--- fetchFinishMatching.php
+++ fetchFinishMatching.php
@@ -14,7 +14,7 @@
 
     $sql = "select * from matching WHERE user_id='".$_SESSION['user_id']."' and turtle_id=''";
     $data = mysqli_query($conn, $sql);
-
+    if (mysqli_num_rows($data) > 0){
     $count = 0;
     $data = array();
     while ($row = $data->fetch_assoc()) {
@@ -33,5 +33,6 @@
         $out = array_values($data);
         $json = json_encode($out, JSON_FORCE_OBJECT);
         echo ($json);
+    }
     }
 ?>   
