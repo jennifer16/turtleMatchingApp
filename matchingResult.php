71cc3802705c6418cdfe55bdc2a1e34a840d3880
natapon <pantuwoing@gmail.com>
2018-05-18 16:03:43 +0700

    matching result

--- matchingResult.php
+++ matchingResult.php
@@ -151,6 +151,7 @@ $result = mysqli_query($conn, $sql);
                          echo "<td align='center'>ยังดำเนินการอยู่</td>";
                          echo "<td>&nbsp;</td>";
                      }else{
+                         echo "<td align='center'>ดำเนินการเสร็จสิ้น</td>";
                         echo "<td align='center'><button type='button' class='btn btn-info' onclick='goToDetail(".$row['id'].");'>ดูผล</button></td>";
                      }
                      
