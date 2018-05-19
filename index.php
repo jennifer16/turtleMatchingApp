53f050054a3b4308c8d2f684cf051407d12ed178
natapon <pantuwoing@gmail.com>
2018-05-19 22:08:31 +0700

    singout

--- index.php
+++ index.php
@@ -110,12 +110,6 @@ if( !isset($_SESSION["user_id"]) ){
 
 
                             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                <ol class="carousel-indicators">
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                </ol>
-
                                 <div class="carousel-inner" role="listbox">
                                     <div class="carousel-item active">
                                         <img src="demo/img/carousel/c-1.jpg" alt="First slide">
