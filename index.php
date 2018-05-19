04a5834202f1da26c9644ce9743a03ab89099c00
natapon <pantuwoing@gmail.com>
2018-05-19 20:58:29 +0700

    all code next is fixed

--- index.php
+++ index.php
@@ -218,7 +218,7 @@ if( !isset($_SESSION["user_id"]) ){
                                 echo "<figcaption>พบโดย:".$userName." ".$userLastname."</figcaption>";
                                 echo "</figure>";
                                 
-                                num++;
+                                $num++;
                             }
                                 
                             }
