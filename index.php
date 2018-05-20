f43e34adf2a2e62732a19af16b574a4aff50601f
natapon <pantuwoing@gmail.com>
2018-05-21 00:44:25 +0700

    fix index

--- index.php
+++ index.php
@@ -107,6 +107,12 @@ if( !isset($_SESSION["user_id"]) ){
 
 
                             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
                                 <div class="carousel-inner" role="listbox">
                                     <div class="carousel-item active">
                                         <img src="demo/img/carousel/c-1.jpg" alt="First slide">
@@ -118,7 +124,12 @@ if( !isset($_SESSION["user_id"]) ){
                                         <img src="demo/img/carousel/c-3.jpg" alt="Third slide">
                                     </div>
                                 </div>
-
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                </a>
                             </div><hr>
 
               <div class="card">
