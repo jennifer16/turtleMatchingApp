93e8ccb74ab530b7504a5dd85f72a575b74912c0
natapon <pantuwoing@gmail.com>
2018-05-20 23:42:08 +0700

    send email

--- sendEmail.php
+++ sendEmail.php
@@ -3,6 +3,5 @@
 	$strSubject = $_POST['subject'];
 	$strHeader = "From: ".$_POST['email'];
 	$strMessage = "ข้อความจาก ".$_POST['name']."\n หมายเลขโทรศัพท์ ".$_POST['mobile']."\n".$_POST['message'];
-	$flgSend = mail($strTo,$strSubject,$strMessage,$strHeader);  // @ = No Show Error //
-	echo $flgSend;
+	mail("pantuwong@gmail.com","My subject",$msg);
 ?>
