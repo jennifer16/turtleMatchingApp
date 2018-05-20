67bafb63b2facc6268e79b3237ae170db438dfd8
natapon <pantuwoing@gmail.com>
2018-05-20 14:54:01 +0700

    try modal label

--- signout.php
+++ signout.php
@@ -1,3 +1,7 @@
 <?php
+
+    $token = $_SESSION['fb_access_token'];
+$url = 'https://www.facebook.com/logout.php?next=https://studioxpert.com/turtleMatchingApp/&access_token='.$token;
+session_destroy();
     header('Location: login.php');
 ?>
