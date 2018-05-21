a5ad1411a73cd6242d0ccad41cb254af658a4e3e
natapon <pantuwoing@gmail.com>
2018-05-21 22:54:41 +0700

    fix all turtle

--- allTurtle.php
+++ allTurtle.php
@@ -140,7 +140,7 @@ $data = mysqli_query($conn, $sql);
               echo "<strong>".$row['turtle_name']."</strong>";
                 
                  
-            $time = strtotime($timest);
+            $time = strtotime($row['turtle_time']);
             $curtime = time();
 
               //$today = time();
