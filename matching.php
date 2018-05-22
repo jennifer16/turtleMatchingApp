e94ef5512b7b7a4e27fbfbdcf03ed65a5f8dcaf5
natapon <pantuwoing@gmail.com>
2018-05-22 12:33:23 +0700

    fix found turtle history

--- matching.php
+++ matching.php
@@ -607,8 +607,8 @@ google.maps.event.addListener( marker, 'dragend', function ( event ) {
     {
         var x = document.getElementById("latitude");
         var y = document.getElementById("longtitude")
-       
-        displayLocation(parseFloat(x.value), parseFloat(y.value);
+       console.log(x.value+","+y.value);
+        displayLocation(parseFloat(x.value), parseFloat(y.value));
     }
     
     
