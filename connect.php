54f01d4c5ee4a912e26178f45ac34b6946bc13d1
natapon <pantuwoing@gmail.com>
2018-05-23 17:53:51 +0700

    resize

--- connect.php
+++ connect.php
@@ -5,5 +5,10 @@ $password = "Tu2tlem@tching";
 $dbname = "turtle";
 $conn = new mysqli($host, $username, $password, $dbname );
 mysqli_set_charset($conn,'utf8');
+
+define('TIMEZONE', 'Asia/Bangkok');
+date_default_timezone_set(TIMEZONE);
+
+mysqli_query($conn, "SET time_zone='+7:00';")
 ?>
 
