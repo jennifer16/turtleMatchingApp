87e722d4dc454ece79c5fae32ab5c83ff2d0d522
natapon <pantuwoing@gmail.com>
2018-05-23 16:54:45 +0700

    resize

--- testResize.php
+++ testResize.php
@@ -2,6 +2,28 @@
     
     $tn = './Turtle/JcYmU56Vww.JPG'
     $save = './Turtle/NaTest.JPG'
-    imagejpeg($tn, $save, 75);
+ 
+function compress($source, $destination, $quality) {
+
+    $info = getimagesize($source);
+
+    if ($info['mime'] == 'image/jpeg') 
+        $image = imagecreatefromjpeg($source);
+
+    elseif ($info['mime'] == 'image/gif') 
+        $image = imagecreatefromgif($source);
+
+    elseif ($info['mime'] == 'image/png') 
+        $image = imagecreatefrompng($source);
+
+    imagejpeg($image, $destination, $quality);
+
+    return $destination;
+}
+
+$source_img = 'source.jpg';
+$destination_img = 'destination .jpg';
+
+$d = compress($tn, $save, 75);
 
 ?>
