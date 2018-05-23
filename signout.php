5932167bca26cbecd75f85aa1f250fc7739728bf
natapon <pantuwoing@gmail.com>
2018-05-23 08:43:39 +0700

    modal

--- signout.php
+++ signout.php
@@ -1,8 +1,19 @@
-<?php
-require_once __DIR__ . '/Facebook/autoload.php'; // change path as needed
 
+<?php
+session_start();
+require_once __DIR__ . '/Facebook/autoload.php';
+require 'connect.php';
+
+$fb = new Facebook\Facebook([
+  'app_id' => '161713021336907',
+  'app_secret' => 'e4dbd79e0e6da4d75019803b487214d2', 
+  'default_graph_version' => 'v2.10',
+  ]);
+
+
+$fb->destroySession();
 
     session_destroy();
  
-    header('Location:'.$url);
+    header('Location:login.php');
 ?>
