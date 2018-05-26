d6ff03bfe17dc50ec65bb8de55d2dd39fc097dfd
natapon <pantuwoing@gmail.com>
2018-05-26 09:30:20 +0700

    facebook noti

--- testFacebookNoti.php
+++ testFacebookNoti.php
@@ -11,50 +11,17 @@ $fb = new \Facebook\Facebook([
 
 $helper = $fb->getCanvasHelper();
 $permissions = []; // optionnal
-try {
-	if (isset($_SESSION['facebook_access_token'])) {
-	$accessToken = $_SESSION['facebook_access_token'];
-	} else {
-  		$accessToken = $helper->getAccessToken();
-	}
-    echo "get token";
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
- 	// When Graph returns an error
- 	echo 'Graph returned an error: ' . $e->getMessage();
-  	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
- 	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  	exit;
- }
 
+$fb->setDefaultAccessToken($_SESSION['fb_access_token']);
 
-    echo "here";
-	if (isset($_SESSION['facebook_access_token'])) {
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	} else {
-		$_SESSION['facebook_access_token'] = (string) $accessToken;
-	  	// OAuth 2.0 client handler
-		$oAuth2Client = $fb->getOAuth2Client();
-		// Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	}
+	
 	// validating the access token
 	try {
 		$request = $fb->get('/me');
         echo "pass validate";
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
         echo "error found";
-		// When Graph returns an error
-		if ($e->getCode() == 190) {
-			unset($_SESSION['facebook_access_token']);
-			$helper = $fb->getRedirectLoginHelper();
-			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-			echo "<script>window.top.location.href='".$loginUrl."'</script>";
-			exit;
-		}
+		
 	} catch(Facebook\Exceptions\FacebookSDKException $e) {
 		// When validation fails or other local issues
 		echo 'Facebook SDK returned an error: ' . $e->getMessage();
