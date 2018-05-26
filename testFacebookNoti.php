f18cd5b5abc7d5fbbfaaaa14902fd3626d233af4
natapon <pantuwoing@gmail.com>
2018-05-26 08:55:29 +0700

    facebook noti

--- testFacebookNoti.php
+++ testFacebookNoti.php
@@ -10,5 +10,5 @@ $fb = new \Facebook\Facebook([
 ]);
 
 	// sending notification to user
-	$sendNotif = $fb->post('/' . $__SESSION['user_id'] . '/notifications', array('href' => '?true=43', 'template' => 'click here for more information!'), $_SESSION['fb_access_token']);
+	$sendNotif = $fb->post('/' . $_SESSION['user_id'] . '/notifications', array('href' => '?true=43', 'template' => 'click here for more information!'), $_SESSION['fb_access_token']);
 ?>
