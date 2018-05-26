da8c331f3a65ec484c9004a967b358a303ba3fd6
natapon <pantuwoing@gmail.com>
2018-05-26 08:53:21 +0700

    facebook noti

---
+++ testFacebookNoti.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+require_once __DIR__ . '/Facebook/autoload.php'; // change path as needed
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '161713021336907',
+  'app_secret' => 'e4dbd79e0e6da4d75019803b487214d2',
+  'default_graph_version' => 'v2.10',
+  //'default_access_token' => '{access-token}', // optional
+]);
+
+	// sending notification to user
+	$sendNotif = $fb->post('/' . $__SESSION['user_id'] . '/notifications', array('href' => '?true=43', 'template' => 'click here for more information!'), $_SESSION['fb_access_token']);
+?>
