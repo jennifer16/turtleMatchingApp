0d54845c324a9fa5a834398c9e777cb264cc7ec9
natapon <pantuwoing@gmail.com>
2018-05-26 09:22:09 +0700

    facebook noti

--- testFacebookNoti.php
+++ testFacebookNoti.php
@@ -28,4 +28,39 @@ try {
   	exit;
  }
 
+if (isset($accessToken)) {
+	if (isset($_SESSION['facebook_access_token'])) {
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	} else {
+		$_SESSION['facebook_access_token'] = (string) $accessToken;
+	  	// OAuth 2.0 client handler
+		$oAuth2Client = $fb->getOAuth2Client();
+		// Exchanges a short-lived access token for a long-lived one
+		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+	}
+	// validating the access token
+	try {
+		$request = $fb->get('/me');
+        echo "pass validate";
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		if ($e->getCode() == 190) {
+			unset($_SESSION['facebook_access_token']);
+			$helper = $fb->getRedirectLoginHelper();
+			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
+			echo "<script>window.top.location.href='".$loginUrl."'</script>";
+			exit;
+		}
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+	
+}
+
+
+
 ?>
