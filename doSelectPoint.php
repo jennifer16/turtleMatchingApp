8f98a732e01fdc194ac7af51e36276fda15accf7
natapon <pantuwoing@gmail.com>
2018-06-13 22:17:38 +0700

    add selectPoint.php

--- doSelectPoint.php
+++ doSelectPoint.php
@@ -11,13 +11,13 @@
   $leftName = "".$turtle_id."_Left";
   $rightName = "".$turtle_id."_Right";
 
-  echo "before first image<br>";
+  
   echo "python delaunay2D_plotDemo.py \"".$leftX."\" \"".$leftY."\" ".$leftName;
   $result = exec("python delaunay2D_plotDemo.py \"".$leftX."\" \"".$leftY."\" ".$leftName);
   echo $result;
 
 
-  //$result = exec("python delaunay2D_plotDemo.py \"".$rightX."\" \"".$rightY."\" ".$rightName);
-  //echo "<br>";
-   //echo $result;
+  $result = exec("python delaunay2D_plotDemo.py \"".$rightX."\" \"".$rightY."\" ".$rightName);
+  echo "<br>";
+   echo $result;
 ?>
