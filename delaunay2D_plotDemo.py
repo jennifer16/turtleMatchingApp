cbae0661c0796b41352c7ae4aae70d0e9e302dfb
natapon <pantuwoing@gmail.com>
2018-06-13 23:05:44 +0700

    fix bug

--- delaunay2D_plotDemo.py
+++ delaunay2D_plotDemo.py
@@ -6,6 +6,7 @@ import numpy as np
 from delaunay2D import Delaunay2D
 
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.tri
 import matplotlib.collections
