a4d26e4918b382ca2b38aaffdf1eca1a14c43196
natapon <pantuwoing@gmail.com>
2018-06-13 14:49:10 +0700

    add selectPoint.php

---
+++ doSelectPoint.php
@@ -0,0 +1,15 @@
+<?php
+  session_start();
+  require 'connect.php';
+  
+  echo $_POST['leftX'];
+
+  echo "<br>";
+ 
+ echo $_POST['leftY'];
+
+  echo "<br>";
+ echo $_POST['rightX'];
+echo $_POST['rightY'];
+
+?>
