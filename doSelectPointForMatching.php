0ad857decebf4d7a9c52285b712b45b6467e499e
natapon <pantuwoing@gmail.com>
2018-06-15 00:59:41 +0700

    doSelectPointForMatching.php

--- doSelectPointForMatching.php
+++ doSelectPointForMatching.php
@@ -19,7 +19,7 @@ if( !isset($_SESSION["user_id"]) ){
   //echo $result;
   //echo "<br>";
 
-  header("Location:doMatching?match_id=".$matchId);
+  header("Location:doMatching.php?match_id=".$matchId);
 
 ?>
 
