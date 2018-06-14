634a3bd116ced67b2bccbfdadd823c313a08f09a
natapon <pantuwoing@gmail.com>
2018-06-14 22:14:36 +0700

    doSelection

--- doSelectPoint.php
+++ doSelectPoint.php
@@ -17,12 +17,12 @@ if( !isset($_SESSION["user_id"]) ){
   $rightName = "".$turtle_id."_Right";
   
   $result = shell_exec("sudo python3 /var/www/html/turtleMatchingApp/delaunay2D_plotDemo.py \"".$leftX."\" \"".$leftY."\" ".$leftName);
-  //echo $result;
+  echo $result;
   echo "<br>";
 
 
   $result = shell_exec("sudo python3 /var/www/html/turtleMatchingApp/delaunay2D_plotDemo.py \"".$rightX."\" \"".$rightY."\" ".$rightName);
-   //echo $result;
+   echo $result;
 
 ?>
 
