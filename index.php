2f75383ca751c3e1351fbb2be508245178f70a0b
natapon <pantuwoing@gmail.com>
2018-06-26 00:25:58 +0700

    separate mesh create

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ $numWaitForMatch = mysqli_num_rows($result);
                         
                         <?php
                             if ($_SESSION['user_role']==1)
-                            { echo "<li><a href='foundTurtleList.php'><i class='zmdi zmdi-layers'></i> เต่าที่พบในธรรมชาติ";
+                            { echo "<li><a href='foundTurtleList.php'><i class='zmdi zmdi-layers'></i> เต่าที่พบในธรรมชาติ &nbsp; ";
                              if($numWaitForMatch>0)  echo "<span class='badge badge-danger'>".$numWaitForMatch."</span></a>";
                              echo "</li>"; }
                         ?>
