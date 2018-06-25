d9cf98b0eabf3cee43861b3f7ac424580a95dae3
natapon <pantuwoing@gmail.com>
2018-06-25 23:23:26 +0700

    separate mesh create

--- doSelectPointRight.php
+++ doSelectPointRight.php
@@ -10,7 +10,7 @@ if( !isset($_SESSION["user_id"]) ){
   $rightY = $_POST['rightY'];
   $turtle_id = $_POST['turtleId'];
   $rightName = "".$turtle_id."_Right";
-
+echo "sudo python3 /var/www/html/turtleMatchingApp/delaunay2D_plotDemo.py \"".$rightX."\" \"".$rightY."\" ".$rightName ;
   $result = shell_exec("sudo python3 /var/www/html/turtleMatchingApp/delaunay2D_plotDemo.py \"".$rightX."\" \"".$rightY."\" ".$rightName);
   echo $result;
  $rightExists = file_exists("./Turtle/".$rightName.".png");
