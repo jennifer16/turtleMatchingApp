9ae5cbcd7a08ecd6e39e7c9fd72b8a6b8ca5669d
natapon <pantuwoing@gmail.com>
2018-06-29 11:29:53 +0700

    fix layout table

--- foundTurtleList.php
+++ foundTurtleList.php
@@ -27,6 +27,10 @@ $data = mysqli_query($conn, $sql);
         <!-- App styles -->
         <link rel="stylesheet" href="css/app.min.css">
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/1.0.7/css/responsive.dataTables.min.css">
+
+  
+
   
     </head>
 
@@ -240,6 +244,8 @@ $data = mysqli_query($conn, $sql);
         <script src="js/app.min.js"></script>
         
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/1.0.7/js/dataTables.responsive.min.js"></script>
+
         
         <script>
             $(document).ready( function () {
