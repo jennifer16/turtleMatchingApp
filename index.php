b9e88e8d6a8c69999a41509a9249a48846305824
natapon <pantuwoing@gmail.com>
2018-11-06 13:45:16 +0700

    fix index.php

--- index.php
+++ index.php
@@ -467,7 +467,7 @@ function myMap() {
 
         echo "var marker = new google.maps.Marker({";
         echo "    position: new google.maps.LatLng(locations[i][0], locations[i][1]),";
-        echo "    icon: './Turtle/'+location[i][4],";
+        echo "    icon: 'Turtle/'+location[i][4],";
         echo "    url:locations[i][3],";
         echo "    label:{ color: '#FFF', text: i.toString() },";
         echo "    title:locations[i][2],";
