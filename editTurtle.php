05f2d18e2e284ff5aa1449cba39d58a6d12299d6
natapon <pantuwoing@gmail.com>
2018-11-19 17:43:02 +0700

    test css

--- editTurtle.php
+++ editTurtle.php
@@ -128,7 +128,7 @@ $data = mysqli_query($conn, $sql);
                 <tr>
                   <th style='text-align: center;'>ชื่อเต่า</th>
                   <th style='text-align: center;'>รหัสไมโครชิพ</th>
-                  <th style='text-align: center;'>TAG</th>
+                  <!-- <th style='text-align: center;'>TAG</th> -->
                   <th>&nbsp;</th>
                   <th>&nbsp;</th>
                 </tr>
@@ -139,7 +139,7 @@ $data = mysqli_query($conn, $sql);
                     echo "<tr>";
                     echo "<td align='center'><a href='turtleDetail.php?id=".$row['turtle_id']."'>".$row['turtle_name']."</a></td>";
                     echo "<td align='center'>".$row['turtle_microchip_code']."</td>";
-                    echo "<td align='center'>".$row['turtle_tag_code']."</td>";
+                  //  echo "<td align='center'>".$row['turtle_tag_code']."</td>";
                    echo "<td align='center'><button type='button' class='btn btn-primary ' onclick='doEditTurtle(".$row['turtle_id'].");'>แก้ไข</button> </td>";
                    echo "<td align='center'><button type='button' class='btn btn-danger' onclick='doDelete(".$row['turtle_id'].");'>ลบ</button></td>";
                     echo "</tr>";
@@ -153,7 +153,7 @@ $data = mysqli_query($conn, $sql);
                 <tfoot>
                 <tr>
                   <th style='text-align: center;'>ชื่อเต่า</th>
-                  <th style='text-align: center;'>รหัสไมโครชิพ</th>
+                  <!--<th style='text-align: center;'>รหัสไมโครชิพ</th>-->
                   <th style='text-align: center;'>TAG</th>
                   <th>&nbsp;</th>
                 </tr>
